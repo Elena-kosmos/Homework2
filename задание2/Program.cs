a5c0563be2c3c116c16fdf6f56c3f4a7b50842c6
Elena <eutereshkova@mail.ru>
2022-09-11 19:54:37 +0300

    Добавили задание 2

---
+++ задание2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string numtext = Convert.ToString(num);
+if(numtext.Length>2)
+{
+    Console.WriteLine("третья цифра" + numtext[2]);
+}
+else 
+{
+  Console.WriteLine("нет 3 цифры");  
+}
+
+
+
