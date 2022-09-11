404ad651754c6127e5a5a537e51b019d93dcb7ad
Elena <eutereshkova@mail.ru>
2022-09-11 19:56:10 +0300

    добавили задание 3

---
+++ задание3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("ведите цифру,обозначающую день недели ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if(dayNumber == 6 || dayNumber ==7) Console.Write("выходной");
+else if (dayNumber<1 || dayNumber>7) Console.Write("это не день недели");
+else Console.Write("это будни");
