ed6acd8223b79d3b7e3e8475d412ec58807f52d9
Elena <eutereshkova@mail.ru>
2022-09-11 19:52:54 +0300

    Добавили задание 1

---
+++ задание1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трехзначное число: ");
+int NumberThreeDigit = Convert.ToInt32(Console.ReadLine());
+string Number = Convert.ToString(NumberThreeDigit);
+Console.WriteLine("вторая цифра этого числа" +Number [1]);
+
